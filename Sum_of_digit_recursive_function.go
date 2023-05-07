2f257d5576918343b897b248b130e556ce549c6a
Aman Khan <amankhanaezakmi@gmail.com>
2023-05-07 16:29:40 +0530

    WAP to print sum of digit of a given number using recursive function.

---
+++ Sum_of_digit_recursive_function.go
@@ -0,0 +1,23 @@
+// WAP to print sum of digit of a given number using recursive function.
+package main
+
+import "fmt"
+
+var summ int = 0
+
+func sum(num int) {
+	var rem int = 0
+	if num > 0 {
+		rem = num % 10
+		summ = summ + rem
+		sum(num / 10)
+	}
+}
+func main() {
+	var num int
+	num = 0
+	fmt.Print("Enter number : ")
+	fmt.Scan(&num)
+	sum(num)
+	fmt.Print(summ)
+}
